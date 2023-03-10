71b31aeff3f175dae57f208559f4a573a589fb66
parthkamal <kamalparth40@gmail.com>
2023-03-10 18:26:22 +0530

    todo-list complete

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ const taskList = document.getElementById("task-list");
 const input = document.getElementById("input");
 
 
-
-
 inputBtn.addEventListener('click',()=>{
     const taskContent = input.value;
     const listLength = localStorage.length;
@@ -20,13 +18,6 @@ const removeAllChildNodes =(parent)=> {
     }
 }
 
-const dispatchApp = ()=> {
-    removeAllChildNodes(taskList);
-    for(let i = 0;i<localStorage.length;i++){
-        addTask(createNewTask(localStorage.getItem(i)));
-    }
-}
-
 
 const createNewTask = (taskContent) => {
     const taskInnerHTML = `<p> ${taskContent}</p>
@@ -38,7 +29,66 @@ const createNewTask = (taskContent) => {
 }
 
 
-// document.getElementById("task-list").appendChild(div);
 const addTask =(task)=>{
     taskList.appendChild(task);
 }
+
+const dispatchApp = ()=> {
+    removeAllChildNodes(taskList);
+    for(let i = 0;i<localStorage.length;i++){
+        addTask(createNewTask(localStorage.getItem(i)));
+    }
+}
+
+
+// const deleteTaskButtonList = document.querySelectorAll(".task-delete-btn");
+// // console.log(deleteTaskButtonList.length);
+// console.log(deleteTaskButtonList);
+
+// for(let i =0 ;i<deleteTaskButtonList.length;i++){
+//     // deleteTaskButtonList[i].addEventListener("", ()=>{
+//     //     localStorage.removeItem(i.toString);
+
+//     //     console.log(`${i} button clicked`);
+//     //     dispatchApp();
+//     // })
+
+//     console.log(`the ${i} element is ${deleteTaskButtonList.item(i)}`)
+// }
+
+
+
+
+var deleteTaskButtonList = document.querySelectorAll(".task-delete-btn");
+// console.log(deleteTaskButtonList.length);
+// console.log(deleteTaskButtonList);
+
+// for(let i =0 ;i<deleteTaskButtonList.length;i++){
+//     deleteTaskButtonList[i].addEventListener("click", ()=>{
+//         localStorage.removeItem(i);
+//         alert('the button is clicked');
+//         console.log(`${i} button clicked`);
+//         dispatchApp();
+//     })
+
+//     console.log(`the ${i} element is ${deleteTaskButtonList[i]}`)
+// }
+
+for(var i=0; i<deleteTaskButtonList.length; i++){
+    deleteTaskButtonList[i].onclick = function(){
+        this.parentNode.remove();
+    }
+}
+
+// for(let i =0 ;i<deleteTaskButtonList.length;i++){
+//     // deleteTaskButtonList[i].addEventListener("", ()=>{
+//     //     localStorage.removeItem(i.toString);
+
+//     //     console.log(`${i} button clicked`);
+//     //     dispatchApp();
+//     // })
+
+//     console.log(`the ${i} element is ${deleteTaskButtonList.item(i)}`)
+// }
+dispatchApp();
+
