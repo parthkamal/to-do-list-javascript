60aa5f3a5bb1323367def11c2fc357197efffd52
parthkamal <kamalparth40@gmail.com>
2023-03-10 17:09:21 +0530

    added the task add functionality with localstorage

--- script.js
+++ script.js
@@ -1,8 +1,44 @@
-console.log(window.localStorage);
+const inputBtn = document.getElementById("input-btn");
+const taskList = document.getElementById("task-list");
+const input = document.getElementById("input");
 
 
-for(let i= 0;i<6;i++){
-    localStorage.setItem(i,`list item ${i}`);
+
+
+inputBtn.addEventListener('click',()=>{
+    const taskContent = input.value;
+    const listLength = localStorage.length;
+    localStorage.setItem(listLength,taskContent);
+    dispatchApp();
+    
+})
+
+
+const removeAllChildNodes =(parent)=> {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
 
-console.log(localStorage);
+const dispatchApp = ()=> {
+    removeAllChildNodes(taskList);
+    for(let i = 0;i<localStorage.length;i++){
+        addTask(createNewTask(localStorage.getItem(i)));
+    }
+}
+
+
+const createNewTask = (taskContent) => {
+    const taskInnerHTML = `<p> ${taskContent}</p>
+    <button class="task-delete-btn">delete</button>`
+    const taskElement = document.createElement("div");
+    taskElement.innerHTML= taskInnerHTML;
+    taskElement.className ="task";
+    return taskElement
+}
+
+
+// document.getElementById("task-list").appendChild(div);
+const addTask =(task)=>{
+    taskList.appendChild(task);
+}
